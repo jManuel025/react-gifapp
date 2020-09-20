d5751d474af95d45db6d2b72c1525153aa9f905b
Manuel Castillo <mac.jmanuel@gmail.com>
2020-09-19 19:45:50 -0500

    add docs

---
+++ docs/precache-manifest.5a50999968a4fb553ce548415c541337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b064f9ba28991f20ad67eacdaa907155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3643bc8495665a2e7b99",
+    "url": "/static/css/main.cf3616f8.chunk.css"
+  },
+  {
+    "revision": "e5eb365363ca112f5f37",
+    "url": "/static/js/2.12ec9c22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12ec9c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3643bc8495665a2e7b99",
+    "url": "/static/js/main.01a70455.chunk.js"
+  },
+  {
+    "revision": "722a304d2cb62627ceb8",
+    "url": "/static/js/runtime-main.3cc8bee4.js"
+  }
+]);
